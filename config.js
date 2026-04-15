bd8351fcb1ff3419a3a2b23f93b5efe72aadc8af
Zyne <fiqitzy11277@gmail.com>
2026-04-15 15:41:02 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  TELEGRAM_TOKEN: "7684047797:AAG8qofbRkoWJUzqFg38cWbGHM2Zt8F_WDY",
+
+  OWNER_ID: "7119529859",
+
+  BOT_NAME: "DDoS_ZyneBot", // tanpa @
+
+  CHANNEL_USERNAME: "TestiByZyne", // tanpa @ dan https://t.me/
+
+  CHANNEL_ID: "-1003765129140" // Wajib! 
+
+};
